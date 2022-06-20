5aef0529cc745f2f1f8bf116cacba70408892092
Sreekanth <sreekanthsivan@gmail.com>
2022-06-20 16:24:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { store } from "./actions/store";
+import { Provider } from "react-redux";
+import DCandidates from './components/DCandidates';
+import { Container } from "@material-ui/core";
+import { ToastProvider } from "react-toast-notifications";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <ToastProvider autoDismiss={true}>
+        <Container maxWidth="lg">
+          <h1>Blood Donors List</h1>
+          <DCandidates />
+        </Container>
+      </ToastProvider>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/actions/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:60671/api/"
+const azureUrl = "http://blooddonorapiservice2022.azurewebsites.net/api/"
+
+
+export default {
+
+    dCandidate(url = azureUrl + 'dcandidate/') {
+        return {
+            fetchAll: () => axios.get(url),
+            fetchById: id => axios.get(url + id),
+            create: newRecord => axios.post(url, newRecord),
+            update: (id, updateRecord) => axios.put(url + id, updateRecord),
+            delete: id => axios.delete(url + id)
+        }
+    }
+}
